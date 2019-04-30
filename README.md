7a04d7ac312589921888563119df8b51c13dc9ac
Chaitanya Maili <chaitanya.maili@gmail.com>
2019-04-30 17:27:24 +0530

    Done the docker compose laravel two tier application.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # docker-laravel
 Docker compose laravel.
+
+# Steps to follow:
+1. docker-compose up -d
+2. docker-compose exec app php artisan config:cache
+3. docker-compose exec db bash
+   1. mysql -uroot -plaravelroot
+   2. GRANT ALL ON laravel.* TO 'laraveluser'@'%' IDENTIFIED BY 'laravelroot';
+   3. FLUSH PRIVILEGES;
+   4. exit;
+4. docker-compose exec app php artisan migrate
---
+++ laravel-app/resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <!-- CSS And JavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
